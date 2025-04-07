6f9b61178216cfb3d7ae4afd289264b561dc11ba
Marcos <thiamath@gmail.com>
2025-04-07 03:09:22 +0200

    Implement FizzBuzz API endpoint

--- src/main/kotlin/org/thiamath/fizzbuzzserver/adapter/inbound/FizzBuzzImpl.kt
+++ src/main/kotlin/org/thiamath/fizzbuzzserver/adapter/inbound/FizzBuzzImpl.kt
@@ -2,19 +2,27 @@ package org.thiamath.fizzbuzzserver.adapter.inbound
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import org.thiamath.fizzbuzzserver.domain.model.FizzBuzzRequest
+import org.thiamath.fizzbuzzserver.domain.port.inbound.FizzBuzzResponse
 import org.thiamath.fizzbuzzserver.domain.port.inbound.GetFizzBuzz
+import org.thiamath.fizzbuzzserver.domain.service.FizzBuzzService
 
 @RestController
-class FizzBuzzImpl : GetFizzBuzz {
+class FizzBuzzImpl(private val fizzBuzzService: FizzBuzzService) : GetFizzBuzz {
     @GetMapping("/fizzbuzz", produces = ["application/json"])
+    @ResponseBody
     override fun execute(
         @RequestParam(name = "int1", defaultValue = "3") int1: Int,
         @RequestParam(name = "int2", defaultValue = "5") int2: Int,
         @RequestParam(name = "limit", defaultValue = "100") limit: Int,
         @RequestParam(name = "str1", defaultValue = "fizz") str1: String,
         @RequestParam(name = "str2", defaultValue = "buzz") str2: String,
-    ) {
-        TODO("Not yet implemented")
-    }
+    ) =
+        FizzBuzzResponse(
+            data = fizzBuzzService.calculateFizzBuzz(
+                FizzBuzzRequest(int1, int2, limit, str1, str2)
+            ).joinToString(", ")
+        )
 }
--- src/main/kotlin/org/thiamath/fizzbuzzserver/domain/port/inbound/GetFizzBuzz.kt
+++ src/main/kotlin/org/thiamath/fizzbuzzserver/domain/port/inbound/GetFizzBuzz.kt
@@ -1,5 +1,9 @@
 package org.thiamath.fizzbuzzserver.domain.port.inbound
 
 interface GetFizzBuzz {
-    fun execute(int1: Int, int2: Int, limit: Int, str1: String, str2: String)
+    fun execute(int1: Int, int2: Int, limit: Int, str1: String, str2: String): FizzBuzzResponse
 }
+
+data class FizzBuzzResponse(
+    val data: String,
+)
